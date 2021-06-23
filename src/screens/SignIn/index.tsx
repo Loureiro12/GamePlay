a5febe2adc759a6934cabfb143a149376b23b134
André Loureiro <contato.loureiro1@gmail.com>
2021-06-22 22:40:33 -0300

    Criando componente Header e profile

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import { ButtonIcon } from "../../components/ButtonIcon";
 import IllustrationImg from "../../assets/illustration.png";
@@ -9,11 +9,6 @@ import { styles } from "./styles";
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       <Image
         source={IllustrationImg}
         style={styles.image}
@@ -22,9 +17,8 @@ export function SignIn() {
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {"\n"}
-          suas jogatinas {"\n"}
-          facilmente
+          Conecte-se {"\n"}e organize suas {"\n"}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
